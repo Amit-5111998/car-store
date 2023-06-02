3101df1217dc2498f95aac95205af09f8a4adeec
amit5111998 <amit5111998@gmail.com>
2023-06-03 01:43:40 +0530

    UI fixes
    UI fixes

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -1,13 +1,27 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "../components/Buttons/Button";
 import { useRouter } from "next/navigation";
 import styles from "./category.module.scss";
 import Image from "next/image";
+import { useCarStore } from "../store";
+
+interface DocumentsState {
+  [key: string]: any;
+}
 
 function Category({ params }: any) {
+  const zustData = useCarStore((state: any) => state.data);
   const router = useRouter();
+  const [documents, setDocuments] = useState<DocumentsState>({});
+  // let category;
 
+  useEffect(() => {
+    if (Object.keys(zustData).length > 0) {
+      let { categories } = zustData;
+      setDocuments(categories[params.category]);
+    }
+  }, [zustData]);
   return (
     <section className={styles.categoryPage}>
       <div className={styles.categoryHeader}>
@@ -42,49 +56,40 @@ function Category({ params }: any) {
           </p>
         </div>
         <h1>Upload any of thse documents</h1>
-        <div className={styles.documentContainer}>
-          <p>State ID</p>
-          <div className={styles.uploadIcons}>
-            <Image
-              src={"/icons/cameraCar.svg"}
-              width={25}
-              height={25}
-              alt="camera"
-            />
-            <Image
-              src={"/icons/cloudUpload.svg"}
-              width={25}
-              height={25}
-              alt="camera"
-            />
-          </div>
-        </div>
-        <div className={styles.horizontalLine}>
-          <span className={styles.horizontalLineText}>or</span>
-        </div>
-        <div className={styles.documentContainer}>
-          <p>Passport</p>
-          <div className={styles.uploadIcons}>
-            <Image
-              src={"/icons/cameraCar.svg"}
-              width={25}
-              height={25}
-              alt="camera"
-            />
-            <Image
-              src={"/icons/cloudUpload.svg"}
-              width={25}
-              height={25}
-              alt="camera"
-            />
-          </div>
-        </div>
+
+        {Object.keys(documents).length > 0 &&
+          Object.keys(documents.documents).map((key, index) => (
+            <>
+              <div className={styles.documentContainer} key={key}>
+                <p>{key}</p>
+                <div className={styles.uploadIcons}>
+                  <Image
+                    src={"/icons/cameraCar.svg"}
+                    width={25}
+                    height={25}
+                    alt="camera"
+                  />
+                  <Image
+                    src={"/icons/cloudUpload.svg"}
+                    width={25}
+                    height={25}
+                    alt="camera"
+                  />
+                </div>
+              </div>
+              {Object.keys(documents.documents).length !== index + 1 && (
+                <div className={styles.horizontalLine}>
+                  <span className={styles.horizontalLineText}>or</span>
+                </div>
+              )}
+            </>
+          ))}
+        <Button
+          cssClass="skipButton"
+          onSubmit={() => router.push("/")}
+          label="Skip for later"
+        />
       </div>
-      <Button
-        cssClass="skipButton"
-        onSubmit={() => router.push("/")}
-        label="Skip for later"
-      />
     </section>
   );
 }
